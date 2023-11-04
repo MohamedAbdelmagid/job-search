502ba01f7a275bfadbf077989e3cb0ec8b571ca9
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2023-11-04 21:51:40 +0200

    Install FontAwesome icons

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -2,6 +2,7 @@
   <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
     <div class="flex h-full items-center px-8">
       <div>
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
         <span><span class="text-brand-green-1">203</span> jobs matched</span>
       </div>
     </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import "@/index.css";
 import JobSearchApp from "@/JobSearchApp.vue";
 
-createApp(JobSearchApp).mount("#app");
+library.add(faSearch)
+
+createApp(JobSearchApp).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
