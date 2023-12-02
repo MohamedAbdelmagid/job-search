75654d0eceb55730ddd6fd6069ed5f7979565acc
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2023-12-02 21:49:32 +0200

    Job search form component

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,19 +1,14 @@
 <template>
   <main>
-    <section class="flex h-screen flex-col bg-yellow-200 pb-20 pt-10">
+    <section class="flex h-screen flex-col pb-20 pt-10">
       <div class="grid grid-cols-12">
-        <div class="col-span-1 col-start-1 border border-blue-700">
-          1 column
-        </div>
-        <div class="col-span-5 col-start-2 border border-blue-700">
+        <div class="col-span-1 col-start-1"></div>
+        <div class="col-span-5 col-start-2">
           <HeadLine />
+          <JobSearchForm />
         </div>
-        <div class="col-span-5 col-start-7 border border-blue-700">
-          5 column
-        </div>
-        <div class="col-span-1 col-start-12 border border-blue-700">
-          1 column
-        </div>
+        <div class="col-span-5 col-start-7"></div>
+        <div class="col-span-1 col-start-12"></div>
       </div>
     </section>
   </main>
@@ -21,11 +16,13 @@
 
 <script>
 import HeadLine from "@/components/HeadLine.vue";
+import JobSearchForm from "./JobSearchForm.vue";
 
 export default {
   name: "Content",
   components: {
     HeadLine,
+    JobSearchForm,
   },
 };
 </script>
---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <form action="">Job Search Form Component</form>
+</template>
+
+<script>
+export default {
+  name: "JobSearchForm",
+}
+</script>
