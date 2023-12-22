54373d962894c5eba062cad405f3371979a57f2a
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2023-12-22 23:05:33 +0200

    JobView Component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 import JobResultsView from "@/views/JobResultsView.vue";
+import JobView from "@/views/JobView.vue";
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: "/jobs/results",
     name: "JobResults",
     component: JobResultsView,
+  },
+  {
+    path: "/jobs/:id",
+    name: "Job",
+    component: JobView,
   },
 ];
 
---
+++ src/views/JobView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Job Page {{ jobID }}</div>
+</template>
+
+<script>
+export default {
+  name: "JobView",
+  computed: {
+    jobID() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
