8cfa4147cbc3f23bd83a64ac919d60449fffd4f3
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2023-12-25 09:52:52 +0200

    fetch jobs & dynamic jobListing

--- src/components/job/JobListing.vue
+++ src/components/job/JobListing.vue
@@ -1,24 +1,37 @@
 <template>
   <li class="mb-7">
-    <router-link to="/jobs/1" class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray">
-      <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
-        <h2 class="mb-2 text-2xl">Vue Developer</h2>
+    <router-link
+      :to="jobLink"
+      class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
+    >
+      <div class="mx-8 border-b border-solid border-brand-gray-2 pb-2 pt-5">
+        <h2 class="mb-2 text-2xl">{{ job.title }}</h2>
         <div class="flex flex-row align-middle">
           <div class="mr-5">
-            <span>SoftBanks</span>
+            <span>{{ job.organization }}</span>
           </div>
           <div>
-            <span>Nasir City, Cairo, Eygpt</span>
+            <ul>
+              <li
+                v-for="location in job.locations"
+                :key="location"
+                class="mr-5 inline-block"
+              >
+                <span>
+                  {{ location }}
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
       <div class="px-8 py-4">
         <ul class="list-disc pl-8">
-          <li>Bachelor's degree or equivalent practical experience</li>
-          <li>5 years of experience in program management.</li>
-          <li>
-            Experience analyzing data through querying database (e.g., SQL)
-            using spreadsheet software, and creating statistical models.
+          <li
+            v-for="qualification in job.minimumQualifications"
+            :key="qualification"
+          >
+            {{ qualification }}
           </li>
         </ul>
       </div>
@@ -28,6 +41,17 @@
 
 <script>
 export default {
-  name: "JobListing"
-}
+  name: "JobListing",
+  props: {
+    job: {
+      type: Object,
+      reuqired: true,
+    },
+  },
+  computed: {
+    jobLink() {
+      return "/jobs/" + this.job.id;
+    },
+  },
+};
 </script>
--- src/components/job/JobListings.vue
+++ src/components/job/JobListings.vue
@@ -1,18 +1,36 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <JobListing />
+      <JobListing
+        v-for="job in jobs"
+        :key="job.id"
+        :job="job"
+      />
     </ol>
   </main>
 </template>
 
 <script>
-import JobListing from './JobListing.vue';
+import JobListing from "./JobListing.vue";
 
 export default {
   name: "JobListings",
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  methods: {
+    async fetchJobs() {
+      const response = await fetch("http://localhost:3000/jobs");
+      this.jobs = await response.json();
+    },
+  },
+  created() {
+    this.fetchJobs();
+  },
   components: {
     JobListing,
-  }
+  },
 };
 </script>
