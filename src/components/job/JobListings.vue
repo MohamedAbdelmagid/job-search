e95b8fc169abea77f881abbe26359cace98060d1
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2023-12-27 19:50:26 +0200

    Pagination - next & previous page

--- src/components/job/JobListings.vue
+++ src/components/job/JobListings.vue
@@ -11,6 +11,24 @@
     <div class="mx-auto mt-8">
       <div class="flex flex-row flex-nowrap">
         <p class="flex-grow text-sm">Page {{ currentPage }}</p>
+
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >
+            Previous
+          </router-link>
+
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+          >
+            Next
+          </router-link>
+        </div>
       </div>
     </div>
   </main>
@@ -18,7 +36,7 @@
 
 <script>
 import JobListing from "./JobListing.vue";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "JobListings",
@@ -29,13 +47,22 @@ export default {
   },
   computed: {
     currentPage() {
-      return Number.parseInt(this.$route.query.page || 1)
+      return Number.parseInt(this.$route.query.page || 1);
+    },
+    previousPage() {
+      const page = this.currentPage - 1;
+      return page >= 1 ? page : undefined;
+    },
+    nextPage() {
+      const page = this.currentPage + 1;
+      const numbersOfPages = this.jobs.length / 10;
+      return page <= numbersOfPages ? page : undefined;
     },
     displayedJobs() {
-      const firstIndex = (this.currentPage - 1) * 10
-      const lastIndex = this.currentPage * 10
-      return this.jobs.slice(firstIndex, lastIndex)
-    }
+      const firstIndex = (this.currentPage - 1) * 10;
+      const lastIndex = this.currentPage * 10;
+      return this.jobs.slice(firstIndex, lastIndex);
+    },
   },
   methods: {
     async fetchJobs() {
--- tests/units/components/job/JobListings.test.js
+++ tests/units/components/job/JobListings.test.js
@@ -11,8 +11,8 @@ describe("JobListings", () => {
     query: {
       page: "1",
       ...queryParams,
-    }
-  })
+    },
+  });
 
   const renderJobListings = ($route = createRoute()) => {
     return render(JobListings, {
@@ -42,5 +42,47 @@ describe("JobListings", () => {
     const listings = await component.findAllByRole("listitem");
 
     expect(listings).toHaveLength(10);
+  });
+
+  describe("When params exclude page number", () => {
+    it("Displays page number 1", () => {
+      const $route = createRoute({ page: undefined });
+      const component = renderJobListings($route);
+
+      const pageNum = component.getByText("Page 1");
+      expect(pageNum).toBeInTheDocument();
+    });
+  });
+
+  describe("When params include page number", () => {
+    it("Displays the same number of the page", () => {
+      const $route = createRoute({ page: "4" });
+      const component = renderJobListings($route);
+
+      const pageNum = component.getByText("Page 4");
+      expect(pageNum).toBeInTheDocument();
+    });
+  });
+
+  describe("When user is on first page", () => {
+    it("Does no show link to previous page", async () => {
+      axios.get.mockResolvedValue({ data: Array(20).fill({}) });
+
+      const component = renderJobListings();
+      await component.findAllByRole("listitem");  // Make sure jobs are rendered
+
+      const previousLink = component.queryByRole("link", { name: /previous/i });
+      expect(previousLink).not.toBeInTheDocument();
+    });
+
+    it("Shows link to next page", async () => {
+      axios.get.mockResolvedValue({ data: Array(20).fill({}) });
+
+      const component = renderJobListings();
+      await component.findAllByRole("listitem");  // Make sure jobs are rendered
+
+      const nextLink = component.queryByRole("link", { name: /next/i });
+      expect(nextLink).toBeInTheDocument();
+    });
   });
 });
