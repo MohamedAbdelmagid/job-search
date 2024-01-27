fa35c38fbb9def14b936be17b084a8917d11fd28
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2024-01-27 17:06:37 +0200

    SubNav test

--- src/components/nav/SubNav.vue
+++ src/components/nav/SubNav.vue
@@ -3,19 +3,27 @@
     <div class="flex h-full items-center px-8">
       <div v-if="onJobResultsPage">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
-        <span><span class="text-brand-green-1">203</span> jobs matched</span>
+        <span>
+          <span class="text-brand-green-1"> {{ filteredJobs.length }}</span>
+          jobs matched
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "pinia";
+import { useJobStore } from "@/stores/job";
+
 export default {
   name: "SubNav",
   computed: {
+    ...mapState(useJobStore, ["filteredJobs"]),
+
     onJobResultsPage() {
-      return this.$route.name === "JobResults"
-    }
-  }
+      return this.$route.name === "JobResults";
+    },
+  },
 };
 </script>
---
+++ tests/units/components/navs/SubNav.test.js
@@ -0,0 +1,54 @@
+import { render } from "@testing-library/vue";
+import { createTestingPinia } from "@pinia/testing";
+
+import SubNav from "@/components/nav/SubNav.vue";
+import { useJobStore } from "@/stores/job";
+
+describe("SubNav", () => {
+  const setUp = (routeName) => {
+    const pinia = createTestingPinia();
+    const jobStore = useJobStore();
+
+    const component = render(SubNav, {
+      global: {
+        plugins: [pinia],
+        mocks: {
+          $route: {
+            name: routeName,
+          },
+        },
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+    });
+
+    return { component, jobStore };
+  };
+
+  describe("When user is on jobs page", () => {
+    it("Displays job count", async () => {
+      const routeName = "JobResults";
+      const { component, jobStore } = setUp(routeName);
+
+      const numbersOfJobs = 16;
+      jobStore.filteredJobs = Array(numbersOfJobs).fill({});
+
+      const jobsCount = await component.findByText(numbersOfJobs);
+      expect(jobsCount).toBeInTheDocument();
+    });
+  });
+
+  describe("When user is NOT on jobs page", () => {
+    it("Does NOT display job count", () => {
+      const routeName = "Home";
+      const { component, jobStore } = setUp(routeName);
+
+      const numbersOfJobs = 16;
+      jobStore.filteredJobs = Array(numbersOfJobs).fill({});
+
+      const jobsCount = component.queryByText(numbersOfJobs);
+      expect(jobsCount).not.toBeInTheDocument();
+    });
+  });
+});
